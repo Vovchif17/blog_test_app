12d7340131e74f47d79dbb7f7c748fa63eea9eb6
vovchif17 <vovchif17@gmail.com>
2025-04-03 06:56:44 -0400

    add auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resource :session
+  delete "/session", to: "sessions#destroy"
+  resources :passwords, param: :token
   root 'posts#index'
   resources :posts do
     resources :comments, only: [:create, :destroy]
